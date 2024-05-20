a930beec2e57febf0539d3b23d09248e5351d690
hammy275 <hammy275@gmail.com>
2024-05-19 23:49:12 -0400

    Simplify Network Flow (closes #48)

--- common/src/main/java/net/blf02/vrapi/VRAPIMod.java
+++ common/src/main/java/net/blf02/vrapi/VRAPIMod.java
@@ -1,5 +1,6 @@
 package net.blf02.vrapi;
 
+import dev.architectury.event.events.client.ClientPlayerEvent;
 import dev.architectury.event.events.common.PlayerEvent;
 import dev.architectury.event.events.common.TickEvent;
 import dev.architectury.platform.Platform;
@@ -7,7 +8,6 @@ import dev.architectury.utils.Env;
 import net.blf02.vrapi.client.ClientSubscriber;
 import net.blf02.vrapi.client.ReflectionConstants;
 import net.blf02.vrapi.client.VRDataGrabber;
-import net.blf02.vrapi.common.CommonSubscriber;
 import net.blf02.vrapi.common.Constants;
 import net.blf02.vrapi.common.network.Network;
 import net.blf02.vrapi.common.network.packets.LeftVRPacket;
@@ -15,6 +15,7 @@ import net.blf02.vrapi.common.network.packets.VRDataPacket;
 import net.blf02.vrapi.common.network.packets.VRRumblePacket;
 import net.blf02.vrapi.common.network.packets.VersionSyncPacket;
 import net.blf02.vrapi.debug.DebugSubscriber;
+import net.blf02.vrapi.server.ServerSubscriber;
 
 import java.util.logging.Logger;
 
@@ -30,13 +31,17 @@ public class VRAPIMod {
         // Client only
         if (Platform.getEnvironment() == Env.CLIENT) {
             TickEvent.PLAYER_POST.register(ClientSubscriber::onPlayerTick);
+            ClientPlayerEvent.CLIENT_PLAYER_QUIT.register(ClientSubscriber::onLogout);
         }
 
         // Common
-        PlayerEvent.PLAYER_QUIT.register(CommonSubscriber::onPlayerDisconnect);
         if (Constants.doDebugging) {
             TickEvent.PLAYER_POST.register(DebugSubscriber::onPlayerTick);
         }
+
+        // Server
+        PlayerEvent.PLAYER_JOIN.register(ServerSubscriber::onPlayerJoin);
+        PlayerEvent.PLAYER_QUIT.register(ServerSubscriber::onPlayerDisconnect);
 
         // Only bother to grab VR Data when on the client-side
         if (Platform.getEnvironment() == Env.CLIENT) {
