b32968cb677fde3ae9d1efc82e56b407aa3216b7
hammy275 <hammy275@gmail.com>
2024-09-11 19:54:49 -0400

    Move Off of Architectury API

--- common/src/main/java/net/blf02/vrapi/debug/DebugSubscriber.java
+++ common/src/main/java/net/blf02/vrapi/debug/DebugSubscriber.java
@@ -3,24 +3,30 @@ package net.blf02.vrapi.debug;
 import net.blf02.vrapi.api.data.IVRData;
 import net.blf02.vrapi.common.VRAPI;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 
 public class DebugSubscriber {
 
     public static void onPlayerTick(Player player) {
-        if (VRAPI.VRAPIInstance.playerInVR(player)) {
-            IVRData hmd = VRAPI.VRAPIInstance.getVRPlayer(player).getHMD();
-            player.level().addParticle(ParticleTypes.SNOWFLAKE,
-                    hmd.position().x, hmd.position().y, hmd.position().z,
-                    0.01, 0.01, 0.01);
-            IVRData left = VRAPI.VRAPIInstance.getVRPlayer(player).getController1();
-            player.level().addParticle(ParticleTypes.ANGRY_VILLAGER,
-                    left.position().x, left.position().y, left.position().z,
-                    0.01, 0.01, 0.01);
-            IVRData right = VRAPI.VRAPIInstance.getVRPlayer(player).getController0();
-            player.level().addParticle(ParticleTypes.FALLING_WATER,
-                    right.position().x, right.position().y, right.position().z,
-                    0.01, 0.01, 0.01);
+        if (VRAPI.VRAPIInstance.playerInVR(player) && player instanceof ServerPlayer sp) {
+            IVRData data = VRAPI.VRAPIInstance.getVRPlayer(player).getHMD();
+            Vec3 pos = data.position().add(data.getLookAngle());
+            sp.serverLevel().sendParticles(ParticleTypes.SNOWFLAKE,
+                    pos.x, pos.y, pos.z, 1,
+                    0.01, 0.01, 0.01, 0.0001);
+            data = VRAPI.VRAPIInstance.getVRPlayer(player).getController1();
+            pos = data.position().add(data.getLookAngle());
+            sp.serverLevel().sendParticles(ParticleTypes.ANGRY_VILLAGER,
+                    pos.x, pos.y, pos.z, 1,
+                    0.01, 0.01, 0.01, 0.0001);
+            data = VRAPI.VRAPIInstance.getVRPlayer(player).getController0();
+            pos = data.position().add(data.getLookAngle());
+            sp.serverLevel().sendParticles(ParticleTypes.FALLING_WATER,
+                    pos.x, pos.y, pos.z, 1,
+                    0.01, 0.01, 0.01, 0.0001);
+            VRAPI.VRAPIInstance.triggerHapticPulse(0, 0.025f, sp);
         }
     }
 }
